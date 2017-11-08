f47924575dea2bfe301f9fc8ee7b0adf1ab52a18
Unknown <administracion@sitek.mx>
2017-11-08 08:56:32 -0700

    Esquema de la base de datos

--- sql/vw_proveedores.sql
+++ sql/vw_proveedores.sql
@@ -1,4 +1,68 @@
-﻿CREATE VIEW [dbo].[vw_proveedores]
+﻿USE [CursoASP]
+GO
+
+drop table Adeudos
+drop table Clientes
+drop table Localidades
+drop table Proveedores
+GO
+
+/****** Object:  Table [dbo].[Proveedores]    Script Date: 08/11/2017 08:50:46 a. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Proveedores](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Nombre] [varchar](250) NOT NULL,
+	[RFC] [varchar](20) NOT NULL,
+	[Moral] [bit] NOT NULL,
+	[localidad_id] [int] NOT NULL,
+ CONSTRAINT [PK_Proveedores] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[Adeudos]    Script Date: 08/11/2017 08:50:46 a. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Adeudos](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[proveedor_id] [int] NOT NULL,
+	[monto] [numeric](10, 2) NOT NULL,
+ CONSTRAINT [PK_Adeudos] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[Localidades]    Script Date: 08/11/2017 08:50:46 a. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Localidades](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[nombre] [varchar](250) NOT NULL,
+	[padre_id] [int] NULL,
+ CONSTRAINT [PK_Localidades] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+/****** Object:  View [dbo].[vw_proveedores]    Script Date: 08/11/2017 08:50:46 a. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE VIEW [dbo].[vw_proveedores]
 AS
 SELECT        dbo.Proveedores.Id, dbo.Proveedores.Nombre, dbo.Proveedores.RFC, dbo.Proveedores.Moral, ISNULL(SUM(dbo.Adeudos.monto), 0) AS monto, dbo.Localidades.nombre AS Localidad, Localidades_1.nombre AS Estado
 FROM            dbo.Proveedores INNER JOIN
@@ -7,4 +71,217 @@ FROM            dbo.Proveedores INNER JOIN
                          dbo.Adeudos ON dbo.Proveedores.Id = dbo.Adeudos.proveedor_id
 GROUP BY dbo.Proveedores.Id, dbo.Proveedores.Nombre, dbo.Proveedores.RFC, dbo.Proveedores.Moral, dbo.Localidades.nombre, Localidades_1.nombre
 
+GO
+/****** Object:  Table [dbo].[Clientes]    Script Date: 08/11/2017 08:50:46 a. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Clientes](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Nombre] [varchar](250) NOT NULL,
+	[Estado] [varchar](100) NULL,
+	[Localidad] [varchar](100) NULL,
+	[Adeudo] [numeric](10, 2) NOT NULL,
+	[Vence] [date] NULL,
+	[RFC] [varchar](20) NOT NULL,
+	[Moral] [bit] NOT NULL,
+ CONSTRAINT [PK_Clientes] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+SET ANSI_PADDING ON
+
+GO
+/****** Object:  Index [IX_Clientes]    Script Date: 08/11/2017 08:50:46 a. m. ******/
+CREATE UNIQUE NONCLUSTERED INDEX [IX_Clientes] ON [dbo].[Clientes]
+(
+	[RFC] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[Clientes] ADD  CONSTRAINT [DF_Clientes_Estado]  DEFAULT ('Chihuahua') FOR [Estado]
+GO
+ALTER TABLE [dbo].[Clientes] ADD  CONSTRAINT [DF_Clientes_Localidad]  DEFAULT ('Chihuahua') FOR [Localidad]
+GO
+ALTER TABLE [dbo].[Clientes] ADD  CONSTRAINT [DF_Clientes_Moral]  DEFAULT ((0)) FOR [Moral]
+GO
+ALTER TABLE [dbo].[Proveedores] ADD  CONSTRAINT [DF_Proveedores_Moral]  DEFAULT ((0)) FOR [Moral]
+GO
+ALTER TABLE [dbo].[Proveedores] ADD  CONSTRAINT [DF_Proveedores_localidad_id]  DEFAULT ((1)) FOR [localidad_id]
+GO
+ALTER TABLE [dbo].[Adeudos]  WITH CHECK ADD  CONSTRAINT [FK_Adeudos_Proveedores] FOREIGN KEY([proveedor_id])
+REFERENCES [dbo].[Proveedores] ([Id])
+GO
+ALTER TABLE [dbo].[Adeudos] CHECK CONSTRAINT [FK_Adeudos_Proveedores]
+GO
+ALTER TABLE [dbo].[Localidades]  WITH CHECK ADD  CONSTRAINT [FK_Localidades_Localidades] FOREIGN KEY([padre_id])
+REFERENCES [dbo].[Localidades] ([Id])
+GO
+ALTER TABLE [dbo].[Localidades] CHECK CONSTRAINT [FK_Localidades_Localidades]
+GO
+ALTER TABLE [dbo].[Proveedores]  WITH CHECK ADD  CONSTRAINT [FK_Proveedores_Localidades] FOREIGN KEY([localidad_id])
+REFERENCES [dbo].[Localidades] ([Id])
+GO
+ALTER TABLE [dbo].[Proveedores] CHECK CONSTRAINT [FK_Proveedores_Localidades]
+GO
+EXEC sys.sp_addextendedproperty @name=N'MS_DiagramPane1', @value=N'[0E232FF0-B466-11cf-A24F-00AA00A3EFFF, 1.00]
+Begin DesignProperties = 
+   Begin PaneConfigurations = 
+      Begin PaneConfiguration = 0
+         NumPanes = 4
+         Configuration = "(H (1[40] 4[12] 2[21] 3) )"
+      End
+      Begin PaneConfiguration = 1
+         NumPanes = 3
+         Configuration = "(H (1 [50] 4 [25] 3))"
+      End
+      Begin PaneConfiguration = 2
+         NumPanes = 3
+         Configuration = "(H (1 [50] 2 [25] 3))"
+      End
+      Begin PaneConfiguration = 3
+         NumPanes = 3
+         Configuration = "(H (4 [30] 2 [40] 3))"
+      End
+      Begin PaneConfiguration = 4
+         NumPanes = 2
+         Configuration = "(H (1 [56] 3))"
+      End
+      Begin PaneConfiguration = 5
+         NumPanes = 2
+         Configuration = "(H (2 [66] 3))"
+      End
+      Begin PaneConfiguration = 6
+         NumPanes = 2
+         Configuration = "(H (4 [50] 3))"
+      End
+      Begin PaneConfiguration = 7
+         NumPanes = 1
+         Configuration = "(V (3))"
+      End
+      Begin PaneConfiguration = 8
+         NumPanes = 3
+         Configuration = "(H (1[56] 4[18] 2) )"
+      End
+      Begin PaneConfiguration = 9
+         NumPanes = 2
+         Configuration = "(H (1 [75] 4))"
+      End
+      Begin PaneConfiguration = 10
+         NumPanes = 2
+         Configuration = "(H (1[66] 2) )"
+      End
+      Begin PaneConfiguration = 11
+         NumPanes = 2
+         Configuration = "(H (4 [60] 2))"
+      End
+      Begin PaneConfiguration = 12
+         NumPanes = 1
+         Configuration = "(H (1) )"
+      End
+      Begin PaneConfiguration = 13
+         NumPanes = 1
+         Configuration = "(V (4))"
+      End
+      Begin PaneConfiguration = 14
+         NumPanes = 1
+         Configuration = "(V (2))"
+      End
+      ActivePaneConfig = 0
+   End
+   Begin DiagramPane = 
+      Begin Origin = 
+         Top = -96
+         Left = 0
+      End
+      Begin Tables = 
+         Begin Table = "Proveedores"
+            Begin Extent = 
+               Top = 6
+               Left = 38
+               Bottom = 136
+               Right = 208
+            End
+            DisplayFlags = 280
+            TopColumn = 2
+         End
+         Begin Table = "Adeudos"
+            Begin Extent = 
+               Top = 0
+               Left = 466
+               Bottom = 113
+               Right = 636
+            End
+            DisplayFlags = 280
+            TopColumn = 0
+         End
+         Begin Table = "Localidades"
+            Begin Extent = 
+               Top = 149
+               Left = 252
+               Bottom = 279
+               Right = 438
+            End
+            DisplayFlags = 280
+            TopColumn = 0
+         End
+         Begin Table = "Localidades_1"
+            Begin Extent = 
+               Top = 102
+               Left = 674
+               Bottom = 215
+               Right = 860
+            End
+            DisplayFlags = 280
+            TopColumn = 0
+         End
+      End
+   End
+   Begin SQLPane = 
+   End
+   Begin DataPane = 
+      Begin ParameterDefaults = ""
+      End
+      Begin ColumnWidths = 9
+         Width = 284
+         Width = 1500
+         Width = 1500
+         Width = 1500
+         Width = 1500
+         Width = 1500
+         Width = 1500
+         Width = 1500
+         Width = 1500
+      End
+   End
+   Begin CriteriaPane = 
+      Begin ColumnWidths = 12
+         Column = 1440
+         Alias = 900
+         Table = 2400
+         Output = 720
+         Append = 1400
+         NewValue = 1170
+         SortType = 1350
+         SortOrder = 1410
+         GroupBy = 1350
+         Filter = 1350
+         Or = 1350
+         Or = 1350
+         Or = 13' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'VIEW',@level1name=N'vw_proveedores'
+GO
+EXEC sys.sp_addextendedproperty @name=N'MS_DiagramPane2', @value=N'50
+      End
+   End
+End
+' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'VIEW',@level1name=N'vw_proveedores'
+GO
+EXEC sys.sp_addextendedproperty @name=N'MS_DiagramPaneCount', @value=2 , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'VIEW',@level1name=N'vw_proveedores'
+GO
+USE [master]
+GO
+ALTER DATABASE [CursoASP] SET  READ_WRITE 
 GO
